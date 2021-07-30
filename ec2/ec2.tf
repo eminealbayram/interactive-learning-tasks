a563fb7d7a2fdac964cca9ad39dd2dad9dcf475f
eminealbayram <albayramemine27@gmail.com>
2021-07-30 04:32:09 +0000

    added correct ami

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                         = "ami-0c2b8ca1dad447f8a" #copied from Amazon from first window when we want to create a new instance.
+  ami                         = "ami-ae6272b8" #copied from Amazon from first window when we want to create a new instance.
   instance_type               = "t2.micro"
   availability_zone           = "us-east-1a"
   associate_public_ip_address = true
