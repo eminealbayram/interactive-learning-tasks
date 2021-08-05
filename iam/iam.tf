d96254ceadcea05cc1b1724bd63d2f499585d85f
eminealbayram <albayramemine27@gmail.com>
2021-08-05 14:13:05 +0000

    IAM user task added

---
+++ iam/iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_user" "bob" {
+  name = "bob"
+  path = "/system/"
+
+  tags = {
+    Team = "DevOps"
+  }
+}
+
+resource "aws_iam_group" "sysusers" {
+  name = "sysusers"
+  path = "/users/"
+}
+
+resource "aws_iam_user_group_membership" "membership" {
+  user = aws_iam_user.bob.name
+
+  groups = [
+    aws_iam_group.sysusers.name,
+  ]
+}
